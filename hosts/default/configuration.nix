32b2f4e1b2d4f26c6110618f46cc496c7c659f3b
p34r1 <undeadgamer279@gmail.com>
2024-09-29 16:56:02 -0400

    edit packages

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -132,31 +132,25 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git  
-    wget
+    alacritty
+    neovim
 
+    git
     lf
     ueberzugpp
-    alacritty
-#    swww
-#    rofi-wayland
+
     librewolf
-#    pkgs.kitty
-#    pkgs.waybar
+
     openrazer-daemon
-    feh
     git-credential-oauth
 
     dmenu
 
-#    font-awesome # FIX (doesn't fix waybar for wtv reason)
-
+    feh
     libnotify
+    dunst
  #   pkgs.emptty
  #   pkgs.lemurs
-    #spotify
-    #spicetify
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
