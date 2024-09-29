fa25138d77ee1fbf10642ead27081681410985d0
p34r1 <undeadgamer279@gmail.com>
2024-09-29 16:09:21 -0400

    reference local source

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -80,29 +80,29 @@
   services.xserver.autorun = false;
   services.xserver.displayManager.startx.enable = true;
 
-  services.xserver.windowManager.dwm.package = pkgs.dwm.override {
-    patches = [
-      # replace hash with the value from `nix-prefetch-url "https://dwm.suckless.org/patches/path/to/patch.diff" | xargs nix hash to-sri --type sha256`
-      (pkgs.fetchpatch {
-        url = "https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-20200508-7b77734.diff";
-        hash = "sha256-+b88csCfyR8cnn95rz6enLELLCoHl00gdBhS1Q/mw3o=";
-      })
+  #services.xserver.windowManager.dwm.package = pkgs.dwm.override {
+  #  patches = [
+  #    # replace hash with the value from `nix-prefetch-url "https://dwm.suckless.org/patches/path/to/patch.diff" | xargs nix hash to-sri --type sha256`
+  #    (pkgs.fetchpatch {
+  #      url = "https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-20200508-7b77734.diff";
+  #      hash = "sha256-+b88csCfyR8cnn95rz6enLELLCoHl00gdBhS1Q/mw3o=";
+  #    })
 
-      (pkgs.fetchpatch {
-        url = "https://dwm.suckless.org/patches/steam/dwm-steam-6.2.diff";
-        hash = "sha256-Lvgyv+h1nMdx+Hnw/r1S2Ubd1eh2fmHyQ65cA934odE=";
-      })
+  #    (pkgs.fetchpatch {
+  #      url = "https://dwm.suckless.org/patches/steam/dwm-steam-6.2.diff";
+  #      hash = "sha256-Lvgyv+h1nMdx+Hnw/r1S2Ubd1eh2fmHyQ65cA934odE=";
+  #    })
 
-      (pkgs.fetchpatch {
-        url = "https://dwm.suckless.org/patches/alpha/dwm-alpha-20230401-348f655.diff";
-        hash = "sha256-Hu5f/7OyZoosILFjBwccqpYipm2hyKzLI6fkq+gyZmQ=";
-      })
-    ];
-  }; 
-
-  #services.xserver.windowManager.dwm.package = pkgs.dwm.overrideAttrs {
-  #  src = /home/pearl/dwm;
+  #    (pkgs.fetchpatch {
+  #      url = "https://dwm.suckless.org/patches/alpha/dwm-alpha-20230401-348f655.diff";
+  #      hash = "sha256-Hu5f/7OyZoosILFjBwccqpYipm2hyKzLI6fkq+gyZmQ=";
+  #    })
+  #  ];
   #}; 
+
+  services.xserver.windowManager.dwm.package = pkgs.dwm.overrideAttrs {
+    src = /home/pearl/dwm;
+  }; 
 
 #  programs.hyprland = {
 #    enable = true;
