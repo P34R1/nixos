ebdede96a586b52d3af0c69ebf66973f832c1095
p34r1 <undeadgamer279@gmail.com>
2024-10-03 20:05:57 -0400

    move config out

--- main/home.nix
+++ main/home.nix
@@ -6,6 +6,8 @@
   imports = [
     # Spicetify
     inputs.spicetify-nix.homeManagerModules.default
+
+    ./waybar.nix
   ];
 
   programs.git = {
@@ -34,37 +36,6 @@
      colorScheme = "mocha";
    };
 
-  programs.waybar = {
-    settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        height = 30;
-        output = [
-          "eDP-1"
-          "HDMI-A-1"
-        ];
-        modules-left = [ "hyprland/workspaces" "hyprland/mode" "wlr/taskbar" ];
-        modules-center = [ "hyprland/window" "custom/hello-from-waybar" ];
-        modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
-
-        "hyprland/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
-        };
-
-        "custom/hello-from-waybar" = {
-          format = "hello {}";
-          max-length = 40;
-          interval = "once";
-          exec = pkgs.writeShellScript "hello-from-waybar" ''
-            echo "from within waybar"
-          '';
-        };
-      };
-    };
-  };
- 
   # Home Manager Config.
   home.username = "pearl";
   home.homeDirectory = "/home/pearl";
