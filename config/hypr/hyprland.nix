0977c0d735fc7c16df4708e383f8047cebe813be
p34r1 <undeadgamer279@gmail.com>
2024-10-04 19:09:26 -0400

    add more binds

--- config/hypr/hyprland.nix
+++ config/hypr/hyprland.nix
@@ -16,8 +16,19 @@
       cursor.no_hardware_cursors = true;
 
       "$mod" = "SUPER";
+      "$TERMINAL" = "foot";
+      "$MENU" = "tofi-drun --drun-launch=true";
+
       bind =
         [
+          "$mod, RETURN, exec, $TERMINAL"
+          "$mod, C, killactive,"
+          "$mod, M, exit,"
+          "$mod, E, exec, $fileManager"
+          "$mod, V, togglefloating,"
+          "$mod, R, exec, $MENU"
+          "$mod, P, pseudo," # dwindle
+          "$mod, J, togglesplit," # dwindle
 #          "$mod, F, exec, firefox"
 #          ", Print, exec, grimblast copy area"
         ]
