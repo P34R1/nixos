b8efcae20f6943e453106018c0f014182f3a2d91
p34r1 <undeadgamer279@gmail.com>
2024-10-03 20:14:50 -0400

    simplify file

--- main/waybar.nix
+++ main/waybar.nix
@@ -1,33 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  programs.waybar = {
-    settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        height = 30;
-        output = [
-          "eDP-1"
-          "HDMI-A-1"
-        ];
-        modules-left = [ "hyprland/workspaces" "hyprland/mode" "wlr/taskbar" ];
-        modules-center = [ "hyprland/window" "custom/hello-from-waybar" ];
-        modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
+  programs.waybar.settings = {
+    mainBar = {
+      layer = "top";
+      position = "top";
+      height = 30;
+      output = [
+        "eDP-1"
+        "HDMI-A-1"
+      ];
+      modules-left = [ "hyprland/workspaces" "hyprland/mode" "wlr/taskbar" ];
+      modules-center = [ "hyprland/window" "custom/hello-from-waybar" ];
+      modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
 
-        "hyprland/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
-        };
+      "hyprland/workspaces" = {
+        disable-scroll = true;
+        all-outputs = true;
+      };
 
-        "custom/hello-from-waybar" = {
-          format = "hello {}";
-          max-length = 40;
-          interval = "once";
-          exec = pkgs.writeShellScript "hello-from-waybar" ''
-            echo "from within waybar"
-          '';
-        };
+      "custom/hello-from-waybar" = {
+        format = "hello {}";
+        max-length = 40;
+        interval = "once";
+        exec = pkgs.writeShellScript "hello-from-waybar" ''
+          echo "from within waybar"
+        '';
       };
     };
   };
