1db13eadab19699af56c0293e338af7a8e29a145
p34r1 <undeadgamer279@gmail.com>
2024-10-04 20:20:03 -0400

    open silently

--- config/hypr/windowrules.nix
+++ config/hypr/windowrules.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "tag +music, initialtitle:(Spotify)" # add dynamic tag `music*` to window spotify
-      "workspace 3, tag:music"
+      "workspace 3 silent, tag:music"
       "suppressevent maximize, class:.*" # You'll probably like this.
     ];
   };
