4c111c715c873e4ad5dee50a37b1d2caf5c636cd
p34r1 <undeadgamer279@gmail.com>
2024-10-06 16:09:40 -0400

    switch to master

--- config/hypr/visuals.nix
+++ config/hypr/visuals.nix
@@ -14,8 +14,8 @@
       # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
       #col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
       #col.inactive_border = rgba(595959aa)
-      col.active_border = "rgba(${base0E}ff) rgba(${base09}ff) 60deg";
-      col.inactive_border = "rgba(${base00}ff)";
+      "col.active_border" = "rgba(${base0E}ff) rgba(${base09}ff) 60deg";
+      "col.inactive_border" = "rgba(${base00}ff)";
 
       # Set to true enable resizing windows by clicking and dragging on borders and gaps
       resize_on_border = false;
@@ -23,7 +23,7 @@
       # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
       allow_tearing = false;
 
-      layout = "dwindle";
+      layout = "master";
     };
 
     # https://wiki.hyprland.org/Configuring/Variables/#decoration
@@ -37,7 +37,7 @@
       drop_shadow = true;
       shadow_range = 4;
       shadow_render_power = 3;
-      col.shadow = "rgba(1a1a1aee)";
+      "col.shadow" = "rgba(1a1a1aee)";
 
       # https://wiki.hyprland.org/Configuring/Variables/#blur
       blur = {
@@ -56,25 +56,28 @@
       # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
       bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
-      animation = "windows, 1, 7, myBezier";
-      animation = "windowsOut, 1, 7, default, popin 80%";
-      animation = "border, 1, 10, default";
-      animation = "borderangle, 1, 8, default";
-      animation = "fade, 1, 7, default";
-      animation = "workspaces, 1, 6, default";
+      animation = [
+        "windows, 1, 7, myBezier"
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "borderangle, 1, 8, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 6, default"
+      ];
     };
 
-     # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-     dwindle = {
-       pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-       preserve_split = true # You probably want this
-     };
+    master = {
+      new_status = "slave";
+      no_gaps_when_only = 1;
+      orientation = "left";
+      smart_resizing = false;
+      drop_at_cursor = true;
+    };
 
-     # https://wiki.hyprland.org/Configuring/Variables/#misc
-     misc = {
-       force_default_wallpaper = -1 # Set to 0 or 1 to disable the anime mascot wallpapers
-       disable_hyprland_logo = false # If true disables the random hyprland logo / anime girl background. :(
-     };
+    # https://wiki.hyprland.org/Configuring/Variables/#misc
+    misc = {
+      force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
+      disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
+    };
   };
 }
