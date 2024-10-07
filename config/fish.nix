0756709e3a4f2eeca406b8e54ddbfd6864c64dc3
p34r1 <undeadgamer279@gmail.com>
2024-10-06 23:11:11 -0400

    plugins

--- config/fish.nix
+++ config/fish.nix
@@ -9,6 +9,28 @@
       gitignore = "curl -sL https://www.gitignore.io/api/$argv";
     };
 
+    plugins = [
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "85f863f20f24faf675827fb00f3a4e15c7838d76";
+          sha256 = "sha256-+FUBM7CodtZrYKqU542fQD+ZDGrd2438trKM0tIESs0=";
+        };
+      }
+
+      {
+        name = "dracula";
+        src = pkgs.fetchFromGitHub {
+          owner = "dracula";
+          repo = "fish";
+          rev = "269cd7d76d5104fdc2721db7b8848f6224bdf554";
+          sha256 = "sha256-Hyq4EfSmWmxwCYhp3O8agr7VWFAflcUe8BUKh50fNfY=";
+        };
+      }
+    ];
+
     interactiveShellInit = ''
       function fish_greeting
         set DIR_COLORSCRIPTS "/home/pearl/.local/share/colorscripts"
