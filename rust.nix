cf18d65a2a5ec29aa92c80faf7c3bb0f0ca7b2ab
p34r1 <undeadgamer279@gmail.com>
2024-10-08 21:26:41 -0400

    fix inputs

--- rust.nix
+++ rust.nix
@@ -1,5 +1,5 @@
-({ pkgs, ... }: {
-  nixpkgs.overlays = [ fenix.overlays.default ];
+({ pkgs, inputs, ... }: {
+  nixpkgs.overlays = [ inputs.fenix.overlays.default ];
   environment.systemPackages = with pkgs; [
     (fenix.complete.withComponents [
       "cargo"
