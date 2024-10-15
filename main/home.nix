33e21f85a0d6e73d75533314183ca8cb72d76a31
p34r1 <undeadgamer279@gmail.com>
2024-10-15 00:12:32 -0400

    change fzf default

--- main/home.nix
+++ main/home.nix
@@ -35,7 +35,22 @@
   programs.eza.enable = true;
 
   # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.fzf.enable
-  programs.fzf.enable = true;
+  programs.fzf = {
+    enable = true;
+
+    defaultCommand = "fd --color=always";
+
+    # https://github.com/PatrickF1/fzf.fish/blob/main/functions/_fzf_wrapper.fish
+    defaultOptions = [
+      "--cycle" # allows jumping between the first and last results, making scrolling faster
+      "--layout=reverse" # lists results top to bottom, mimicking the familiar layouts of git log, history, and env
+      "--border" # shows where the fzf window begins and ends
+      "--height=90%" # leaves space to see the current command and some scrollback, maintaining context of work
+      "--preview-window=wrap" # wraps long lines in the preview window, making reading easier
+      "--marker=\"*\"" # makes the multi-select marker more distinguishable from the pointer (since both default to >)
+      "--ansi" # enable ansi color code parsing
+    ];
+  };
 
   # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.jq.enable
   programs.jq.enable = true;
@@ -50,8 +65,7 @@
 
   programs.direnv = {
     enable = true;
-    enableBashIntegration = false;
-    #    enableFishIntegration = true;
+
     config = {
       hide_env_diff = true;
     };
