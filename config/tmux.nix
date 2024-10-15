8f3611d233964560cc37d7b8f9f5870613231ab8
p34r1 <undeadgamer279@gmail.com>
2024-10-14 20:37:44 -0400

    edit tmux configuration

--- config/tmux.nix
+++ config/tmux.nix
@@ -5,6 +5,30 @@
   programs.tmux = {
     enable = true;
 
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/tools/misc/tmux/default.nix
+    package = pkgs.tmux.override ({
+      withSixel = true;
+    });
+
     shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    mouse = true;
+    prefix = "C-Space";
+    baseIndex = 1;
+    escapeTime = 10;
+
+    extraConfig = ''
+      set -g allow-passthrough all
+      set -ga update-environment TERM
+      set -ga update-environment TERM_PROGRAM
+    '';
+
+    plugins = with pkgs; [
+      {
+        # https://github.com/egel/tmux-gruvbox
+        plugin = tmuxPlugins.gruvbox;
+        extraConfig = "set -g @tmux-gruvbox 'dark'";
+      }
+    ];
   };
 }
