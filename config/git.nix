f412e4b3582b34bd0cabb9f52d8ed09d52929fd9
p34r1 <undeadgamer279@gmail.com>
2024-10-15 21:43:24 -0400

    add log

--- config/git.nix
+++ config/git.nix
@@ -29,6 +29,8 @@
       # https://www.youtube.com/watch?v=xN1-2p06Urc
       pr = "pull --rebase";
       amend = "commit -a --amend --no-edit";
+
+      lo = "log --graph --topo-order --pretty='%w(100,0,6)%C(yellow)%h%C(bold)%C(black)%d %C(cyan)%ar %C(green)%an%n%C(bold)%C(white)%s %N' --abbrev-commit";
     };
   };
 }
