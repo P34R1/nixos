84c2926110e3b1bd2ce9635da59aadd4a3a5df59
p34r1 <undeadgamer279@gmail.com>
2024-10-19 12:34:14 -0400

    add documentation

--- config/default.nix
+++ config/default.nix
@@ -3,7 +3,7 @@
 {
   options = {
     user = lib.mkOption {
-        type = lib.types.str;
+      type = lib.types.str;
     };
   };
 
@@ -19,6 +19,9 @@
     #./tmux/tmux.nix
     #./dunst/dunst.nix
 
+    # https://discourse.nixos.org/t/configuring-a-module-alias-for-home-manager/12914/2
+    # This creates an alias hm = home-manager.users.${config.user} (pearl)
+    # Use as config.hm.packages.git = { enable = true; };
     (lib.mkAliasOptionModule ["hm"] ["home-manager" "users" config.user])
   ];
 }
