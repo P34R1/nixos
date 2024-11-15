0e35b3e7027e90f55661fe3a6c1b5664173a8e40
pearl <undeadgamer279@gmail.com>
2024-11-15 18:43:43 -0500

    add tags

--- config/features/alacritty.nix
+++ config/features/alacritty.nix
@@ -37,30 +37,30 @@
 
         # Normal/regular colors (color palette 0-7)
         colors = with config.colours; {
-          primary.background = "${base00}";
-          primary.foreground = "${base06}";
+          primary.background = "#${base00}";
+          primary.foreground = "#${base06}";
 
           normal = {
-            black = "${base01}"; # black
-            red = "${base08}"; # red
-            green = "${base0B}"; # green
-            yellow = "${base09}"; # yellow
-            blue = "${base0D}"; # blue
-            purple = "${base0E}"; # purple
-            cyan = "${base0C}"; # aqua/cyan
-            white = "${base07}"; # white
+            black = "#${base01}"; # black
+            red = "#${base08}"; # red
+            green = "#${base0B}"; # green
+            yellow = "#${base09}"; # yellow
+            blue = "#${base0D}"; # blue
+            purple = "#${base0E}"; # purple
+            cyan = "#${base0C}"; # aqua/cyan
+            white = "#${base07}"; # white
           };
 
           # Bright colors (color palette 8-15)
           bright = {
-            black = "${base04}";
-            red = "${base12}"; # bright red
-            green = "${base14}"; # green
-            yellow = "${base13}"; # yellow
-            blue = "${base16}"; # blue
-            purple = "${base17}"; # purple
-            cyan = "${base15}"; # aqua/cyan
-            white = "${base07}"; # white
+            black = "#${base04}";
+            red = "#${base12}"; # bright red
+            green = "#${base14}"; # green
+            yellow = "#${base13}"; # yellow
+            blue = "#${base16}"; # blue
+            purple = "#${base17}"; # purple
+            cyan = "#${base15}"; # aqua/cyan
+            white = "#${base07}"; # white
           };
         };
       };
