24009e07d1c019c41c56024dac7828854b1302e6
pearl <undeadgamer279@gmail.com>
2024-12-31 15:15:43 -0500

    hypr window rules

--- config/features/hypr/windowrules.nix
+++ config/features/hypr/windowrules.nix
@@ -4,12 +4,13 @@
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "workspace 4 silent, class:vesktop"
+      "workspace special:scratchpad silent, title:App" # for bevy (i'll improve on this later)
 
       "suppressevent maximize, class:.*" # You'll probably like this.
     ];
 
     workspace = [
-      "special:scratchpad, on-created-empty:[float; size 700 1015; move 1200 42] foot"
+      # "special:scratchpad, on-created-empty:[float; size 700 1015; move 1200 42] foot"
     ];
   };
 }
