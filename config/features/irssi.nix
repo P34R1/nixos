a9280a26868cede982111e2ea8b14a17df17a8e3
pearl <undeadgamer279@gmail.com>
2025-02-17 19:26:06 -0500

    irssi configurations

--- config/features/irssi.nix
+++ config/features/irssi.nix
@@ -9,6 +9,34 @@
     # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.irssi.enable
     programs.irssi = {
       enable = true;
+
+      extraConfig = ''
+        settings = {
+          core = {
+            real_name = "pearl";
+          };
+        };
+      '';
+
+      networks = {
+        liberachat = {
+          nick = "pear1";
+          saslExternal = true;
+
+          server = {
+            address = "irc.libera.chat";
+            port = 6697;
+            autoConnect = true;
+
+            ssl.certificateFile = "${config.hm.home.homeDirectory}/.irssi/certs/libera.pem";
+          };
+
+          channels = {
+            nixos.autoJoin = false;
+            edctf.autoJoin = true;
+          };
+        };
+      };
     };
   };
 }
