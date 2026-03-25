ba46ae00e604076422436f905bc5288617d2e040
pearl <vincent.fortin279@gmail.com>
2026-03-25 15:59:18 -0400

    flake upd

--- flake.nix
+++ flake.nix
@@ -3,13 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
-    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
 
-    nvim.url = "github:P34R1/nvim";
-    dwm.url = "github:P34R1/dwm";
-    # slstatus.url = "github:P34R1/slstatus";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+    import-tree.url = "github:vic/import-tree";
+
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nvim = {
+      url = "github:P34R1/nvim";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    dwm = {
+      url = "github:P34R1/dwm";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
@@ -25,15 +39,6 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-flatpak.url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
-
-    # nix-colors.url = "github:misterio77/nix-colors";
-
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
