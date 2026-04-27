a9847fe0ef740fde2d9af9abb4d7d4a5825c9ec5
pearl <vincent.fortin279@gmail.com>
2026-04-26 20:14:46 -0400

    add authorized ssh key

--- modules/features/system/openssh.nix
+++ modules/features/system/openssh.nix
@@ -16,6 +16,7 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH97h8jvSFDbZPR4/KKhAUf8mtrgeIqWGzB9S0ATunDs undeadgamer279@gmail.com"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCAtzH4xk6DyVHj61pl4VzLS2uFaG8s45Xr6u0uX2MA undeadgamer279@gmail.com"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICvavh5rpoj12Ywi5lXT6hCn38KZuZaU8Ln5Rh3OxsPb vincent.fortin279@gmail.com"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHZh8j4e/K05HD4v+SsKNdcFw+k6Y1kfb/CHIUfjWwR vincent.fortin279@gmail.com"
         ];
 
         services.openssh = {
