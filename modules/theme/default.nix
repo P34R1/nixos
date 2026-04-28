a3eafc7f249a74e0c073e843d9ddd0928dc5a77d
pearl <vincent.fortin279@gmail.com>
2026-04-28 15:31:53 -0400

    simplify themeNoHash

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -1,15 +1,10 @@
-{ self, ... }:
+{ self, lib, ... }:
 
 let
-  theme = self.gruvbox;
-  stripHash =
-    str:
-    if builtins.substring 0 1 str == "#" then
-      builtins.substring 1 (builtins.stringLength str - 1) str
-    else
-      str;
+  stripHashes = builtins.mapAttrs (_: lib.removePrefix "#");
 in
 {
-  flake.theme = theme;
-  flake.themeNoHash = builtins.mapAttrs (_: v: stripHash v) theme;
+  flake.theme = self.gruvbox;
+
+  flake.themeNoHash = stripHashes self.theme;
 }
