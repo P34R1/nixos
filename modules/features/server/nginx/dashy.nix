b8b081d0d917e2ed0ce0b5825c670d3cf1eb43cd
pearl <vincent.fortin279@gmail.com>
2026-05-05 15:38:27 -0400

    add dashy settings

--- modules/features/server/nginx/dashy.nix
+++ modules/features/server/nginx/dashy.nix
@@ -2,8 +2,72 @@
 
 {
   flake.nixosModules.nginxDashy =
-    { config, lib, ... }:
+    { config, pkgs, ... }:
+    let
+      section = name: items: {
+        name = name;
+        items = items;
+      };
+
+      item = title: icon: url: {
+        title = title;
+        icon = icon;
+        url = url;
+      };
+
+      nav = title: path: {
+        title = title;
+        path = path;
+      };
+
+      # https://dashboardicons.com/icons
+      dash-icon = name: "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/${name}.svg";
+
+      # https://dashy.to/docs/configuring/
+      settings = {
+        appConfig = {
+          theme = "dashy-docs";
+          preventWriteToDisk = true;
+          preventLocalSave = true;
+          disableConfiguration = false;
+          hideComponents = {
+            hideSearch = true;
+            hideSettings = true;
+            hideFooter = true;
+          };
+        };
+
+        pageInfo = {
+          title = "smegmail";
+          navLinks = [
+            (nav "Home" "/")
+            (nav "Source" "https://github.com/P34R1/nixos/blob/main/modules/features/server/nginx/dashy.nix")
+          ];
+        };
+
+        sections = [
+          (section "Services" [
+            (item "Nextcloud" (dash-icon "nextcloud") "https://cloud.smegmail.org/")
+            (item "slskd" (dash-icon "slskd") "/slskd/")
+            (item "Mail" (dash-icon "gmail") "https://mail.google.com/mail/u/2/")
+          ])
+
+          (section "Hosting" [
+            (item "Gateway" "fas fa-wifi" "https://10.0.0.1/")
+            (item "Cloudflare" (dash-icon "cloudflare") "https://dash.cloudflare.com/")
+          ])
+
+          (section "External" [
+            (item "Github" "fab fa-github" "https://github.com/P34R1/")
+          ])
+        ];
+      };
+    in
     {
+      # uncomment to avoid recompilation
+      # services.nginx.virtualHosts.${config.services.dashy.virtualHost.domain}.locations."= /conf.yml".alias =
+      #   pkgs.writers.writeYAML "dashy-conf.yml" settings;
+
       services.dashy = {
         enable = true;
         virtualHost = {
@@ -11,7 +75,7 @@
           domain = "www.${config.nginx.domain}";
         };
 
-        settings = { };
+        settings = settings;
       };
     };
 }
